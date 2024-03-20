344d6ffe0b5e951aa505168c3c71dccd1068ce5e
Ana Luiza <aninhat17@gmail.com>
2024-03-20 12:06:34 -0300

    Módulo 5

--- conta.py
+++ conta.py
@@ -1,4 +1,3 @@
-
 class Conta():
 
 
@@ -17,3 +16,21 @@ class Conta():
     
     def saque (self, valor):
         self.__saldo -= valor
+
+    def tranfere (self, valor, conta_destino):
+        self.saque(valor)
+        conta_destino.deposito(valor)
+
+
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def get_titular(self):
+        return self.__titular
+
+
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite
